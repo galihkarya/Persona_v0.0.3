6c9b6baacc27232fefbd3b09c2e06592bd49093f
Galih Karya Gemilang <galihkaryagemilang2001@gmail.com>
2024-03-18 13:26:28 +0700

    Adding pages

---
+++ src/pages/index.tsx
@@ -0,0 +1,39 @@
+import AddStudentDataPage from './AddStudentDataPage';
+import CameraPage from './CameraPage';
+import CheckPhotoPage from './CheckPhotoPage';
+import GroupListPage from './GroupListPage';
+import HomePage from './HomePage/';
+import HomePageLogedIn from './HomePageLoggedIn';
+import LoginPage from './LoginPage';
+import ProfilePage from './ProfilePage';
+import RegistName from './Regist1Name';
+import RegistRole from './Regist2Role';
+import RegistInstitutionBK from './Regist3InstitutionBK';
+import RegistInstitutionWK from './Regist3InstitutionWK';
+import RegistAccount from './Regist4Account';
+import ReminderPage from './ReminderPage';
+import ResultListPage from './ResultListPage';
+import ResultPage from './ResultPage';
+import Splash from './SplashScreen';
+import Tabs from '../components/BottomNavigation';
+
+export {
+  AddStudentDataPage,
+  CameraPage,
+  GroupListPage,
+  HomePage,
+  HomePageLogedIn, 
+  LoginPage,
+  ProfilePage,
+  RegistName,
+  RegistRole, 
+  RegistInstitutionBK,
+  RegistInstitutionWK, 
+  RegistAccount, 
+  ReminderPage,
+  ResultListPage,
+  ResultPage,
+  Splash, 
+  CheckPhotoPage, 
+  Tabs
+};
