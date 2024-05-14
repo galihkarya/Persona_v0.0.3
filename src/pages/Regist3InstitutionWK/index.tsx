49272b49a14dbdfadc4514b257f6302665b14f8a
Galih Karya Gemilang <galihkaryagemilang2001@gmail.com>
2024-05-14 13:07:15 +0700

    Update index.tsx
    optimizing API integration

--- src/pages/Regist3InstitutionWK/index.tsx
+++ src/pages/Regist3InstitutionWK/index.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import {
   View,
   Text,
@@ -7,15 +7,51 @@ import {
   StyleSheet,
   TextInput,
   ScrollView,
+  ToastAndroid,
 } from 'react-native';
+import api from '../../API/UserApi';
+import {Dropdown, IDropdownRef} from 'react-native-element-dropdown';
 
-const RegistInstitutionBK = ({navigation, route}:any) => {
+const RegistInstitutionBK = ({navigation, route}: any) => {
   const {full_name, role} = route.params;
   const [institute_code, setInstitute_code] = useState('');
+  const [institute_name, setInstitute_name] = useState();
+  const [institute_id, setInstitute_id] = useState();
+  const [groupIdSelected, setGroupIdSelected] = useState<any>('');
+  const [groupName, setGroupName] = useState('');
+
+  const [classList, setClassList] = useState<any>([]);
+  const [schoolIsTrue, setSchoolIsTrue] = useState<boolean>(false)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
 
   const onPressHandler = () => {
-    navigation.navigate('RegistAccount', {full_name, role, institute_code});
+    setIsLoading(true)
+    if (groupIdSelected == ''){
+    const getInstituteByCode = async () => {
+      await api
+        .get(`/api/v1/institute/code/${institute_code}`)
+        .then(async ({data}) => {
+          console.log('ini', data.name);
+          if (data === null) ToastAndroid.show('Kode sekolah tidak valid', ToastAndroid.LONG);
+          else {
+            setSchoolIsTrue(true);
+            const group = data.groups.map(({id: value, name: label}: any) => ({value, label,}));
+            setClassList(group);
+            console.log('ini bawahnya', data.name)
+            setInstitute_name(data.name)
+            setInstitute_id(data.id)
+          }
+        })
+        .finally(() => setIsLoading(false));
+    };
+    getInstituteByCode();
+    
   }
+    else if (groupIdSelected != '') {
+      // console.log(full_name, role, institute_code, institute_name, groupIdSelected, institute_id);
+      navigation.navigate('RegistAccount', {full_name, role, institute_code, institute_name, groupIdSelected, institute_id});}
+  };
+  const ref = useRef<IDropdownRef>(null);
 
   return (
     <ScrollView>
@@ -30,7 +66,7 @@ const RegistInstitutionBK = ({navigation, route}:any) => {
           source={require('../../../assets/icons/icon_arrowLeft.png')}
         />
       </TouchableOpacity>
-      <View style={{marginTop: 50, marginBottom: 30, alignItems: 'center'}}>
+      <View style={{marginTop: schoolIsTrue ? 0 : 50, marginBottom: 30, alignItems: 'center'}}>
         <Image
           style={Styles.imageCenter}
           source={require('../../../assets/images/school.png')}
@@ -39,11 +75,37 @@ const RegistInstitutionBK = ({navigation, route}:any) => {
         <Text style={Styles.head2}>Masukkan kode sekolah</Text>
       </View>
       <View style={{margin: 20}}>
-        <TextInput style={Styles.input} placeholder="Kode sekolah"  value={institute_code} onChangeText={(val_institute_code) => setInstitute_code(val_institute_code)}/>
+        <TextInput
+          style={[{ marginVertical: schoolIsTrue ? 0 : 8}, Styles.input]}
+          placeholder="Kode sekolah"
+          value={institute_code}
+          autoCapitalize='characters'
+          editable={schoolIsTrue ? false : true}
+          onChangeText={val_institute_code =>
+            setInstitute_code(val_institute_code)
+          }
+        />
+        {(schoolIsTrue && 
+          <Dropdown
+          ref={ref}
+          style={Styles.dropdown}
+          placeholderStyle={Styles.placeholderStyle}
+          selectedTextStyle={Styles.selectedTextStyle}
+          data={classList}
+          maxHeight={150}
+          labelField="label"
+          valueField="value"
+          placeholder="Pilih grup kelas"
+          value={groupIdSelected}
+          onChange={item => {
+            setGroupIdSelected(item.value);
+          }}
+        />
+        )}
         <TouchableOpacity
-          style={[Styles.button, {opacity: institute_code === '' ? 0.5 : 1}]}
+          style={[Styles.button, {opacity: institute_code === '' || isLoading == true ? 0.5 : 1}]}
           onPress={onPressHandler}
-          disabled={ institute_code === '' }>
+          disabled={institute_code === '' || isLoading == true}>
           <Text style={Styles.textButton}>lanjut ...</Text>
         </TouchableOpacity>
       </View>
@@ -80,19 +142,36 @@ const Styles = StyleSheet.create({
     elevation: 8,
     shadowColor: '#00000050',
     paddingHorizontal: 20,
-    marginVertical: 8,
   },
   button: {
     backgroundColor: '#CC3663',
-    marginVertical: 25,
     verticalAlign: 'bottom',
     borderRadius: 15,
+    marginVertical: 25
   },
   textButton: {
     textAlign: 'center',
     color: '#FFFFFF',
     paddingVertical: 15,
     fontSize: 16,
+  },
+  dropdown: {
+    height: 50,
+    backgroundColor: '#FFFFFF',
+    paddingHorizontal: 20,
+    borderRadius: 13,
+    flex: 1,
+    marginVertical: 20, 
+    elevation: 8,  
+    shadowColor: '#00000050',
+  },
+  placeholderStyle: {
+    fontSize: 14,
+    color: 'black'
+  },
+  selectedTextStyle: {
+    fontSize: 14,
+    color: 'black'
   },
 });
 
