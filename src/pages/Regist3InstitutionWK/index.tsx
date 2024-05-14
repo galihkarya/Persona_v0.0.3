8c1c7cd04881b5882ba937c796e167df885f3f34
Galih Karya Gemilang <galihkaryagemilang2001@gmail.com>
2024-05-14 15:29:41 +0700

    Update index.tsx
    optimizing UX and API inetgration

--- src/pages/Regist3InstitutionWK/index.tsx
+++ src/pages/Regist3InstitutionWK/index.tsx
@@ -18,38 +18,49 @@ const RegistInstitutionBK = ({navigation, route}: any) => {
   const [institute_name, setInstitute_name] = useState();
   const [institute_id, setInstitute_id] = useState();
   const [groupIdSelected, setGroupIdSelected] = useState<any>('');
-  const [groupName, setGroupName] = useState('');
 
   const [classList, setClassList] = useState<any>([]);
-  const [schoolIsTrue, setSchoolIsTrue] = useState<boolean>(false)
-  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [schoolIsTrue, setSchoolIsTrue] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const onPressHandler = () => {
-    setIsLoading(true)
-    if (groupIdSelected == ''){
-    const getInstituteByCode = async () => {
-      await api
-        .get(`/api/v1/institute/code/${institute_code}`)
-        .then(async ({data}) => {
-          console.log('ini', data.name);
-          if (data === null) ToastAndroid.show('Kode sekolah tidak valid', ToastAndroid.LONG);
-          else {
-            setSchoolIsTrue(true);
-            const group = data.groups.map(({id: value, name: label}: any) => ({value, label,}));
-            setClassList(group);
-            console.log('ini bawahnya', data.name)
-            setInstitute_name(data.name)
-            setInstitute_id(data.id)
-          }
-        })
-        .finally(() => setIsLoading(false));
-    };
-    getInstituteByCode();
-    
-  }
+    setIsLoading(true);
+    if (groupIdSelected == '') {
+      const getInstituteByCode = async () => {
+        await api
+          .get(`/api/v1/institute/code/${institute_code}`)
+          .then(async ({data}) => {
+            console.log(data);
+            if (data === null)
+              ToastAndroid.show('Kode sekolah tidak valid', ToastAndroid.LONG);
+            else {
+              setSchoolIsTrue(true);
+              const group = data.groups.map(
+                ({id: value, name: label}: any) => ({value, label}),
+              );
+              setClassList(group);
+              setInstitute_name(data.name);
+              setInstitute_id(data.id);
+            }
+          })
+          .finally(() => setIsLoading(false));
+      };
+      getInstituteByCode();
+    } 
     else if (groupIdSelected != '') {
       // console.log(full_name, role, institute_code, institute_name, groupIdSelected, institute_id);
-      navigation.navigate('RegistAccount', {full_name, role, institute_code, institute_name, groupIdSelected, institute_id});}
+      const group = classList.find((group:any) => group.value === groupIdSelected).label;
+      console.log(group);
+      navigation.navigate('RegistAccount', {
+        full_name,
+        role,
+        institute_code,
+        institute_name,
+        groupIdSelected,
+        institute_id,
+        group_name: group
+      });
+    }
   };
   const ref = useRef<IDropdownRef>(null);
 
@@ -66,7 +77,12 @@ const RegistInstitutionBK = ({navigation, route}: any) => {
           source={require('../../../assets/icons/icon_arrowLeft.png')}
         />
       </TouchableOpacity>
-      <View style={{marginTop: schoolIsTrue ? 0 : 50, marginBottom: 30, alignItems: 'center'}}>
+      <View
+        style={{
+          marginTop: schoolIsTrue ? 0 : 50,
+          marginBottom: 30,
+          alignItems: 'center',
+        }}>
         <Image
           style={Styles.imageCenter}
           source={require('../../../assets/images/school.png')}
@@ -76,36 +92,48 @@ const RegistInstitutionBK = ({navigation, route}: any) => {
       </View>
       <View style={{margin: 20}}>
         <TextInput
-          style={[{ marginVertical: schoolIsTrue ? 0 : 8}, Styles.input]}
+          style={[{marginVertical: schoolIsTrue ? 0 : 8}, Styles.input]}
           placeholder="Kode sekolah"
           value={institute_code}
-          autoCapitalize='characters'
+          autoCapitalize="characters"
           editable={schoolIsTrue ? false : true}
           onChangeText={val_institute_code =>
             setInstitute_code(val_institute_code)
           }
         />
-        {(schoolIsTrue && 
+        {schoolIsTrue && (
           <Dropdown
-          ref={ref}
-          style={Styles.dropdown}
-          placeholderStyle={Styles.placeholderStyle}
-          selectedTextStyle={Styles.selectedTextStyle}
-          data={classList}
-          maxHeight={150}
-          labelField="label"
-          valueField="value"
-          placeholder="Pilih grup kelas"
-          value={groupIdSelected}
-          onChange={item => {
-            setGroupIdSelected(item.value);
-          }}
-        />
+            ref={ref}
+            style={Styles.dropdown}
+            placeholderStyle={Styles.placeholderStyle}
+            selectedTextStyle={Styles.selectedTextStyle}
+            data={classList}
+            maxHeight={150}
+            labelField="label"
+            valueField="value"
+            placeholder="Pilih grup kelas"
+            value={groupIdSelected}
+            onChange={item => {
+              setGroupIdSelected(item.value);
+            }}
+          />
         )}
         <TouchableOpacity
-          style={[Styles.button, {opacity: institute_code === '' || isLoading == true ? 0.5 : 1}]}
+          style={[
+            Styles.button,
+            {
+              opacity:
+                institute_code === '' ||
+                (schoolIsTrue && groupIdSelected == '') ||
+                isLoading
+                  ? 0.5
+                  : 1,
+            },
+          ]}
           onPress={onPressHandler}
-          disabled={institute_code === '' || isLoading == true}>
+          disabled={
+            institute_code === '' || (isLoading && groupIdSelected == '')
+          }>
           <Text style={Styles.textButton}>lanjut ...</Text>
         </TouchableOpacity>
       </View>
@@ -147,7 +175,7 @@ const Styles = StyleSheet.create({
     backgroundColor: '#CC3663',
     verticalAlign: 'bottom',
     borderRadius: 15,
-    marginVertical: 25
+    marginVertical: 25,
   },
   textButton: {
     textAlign: 'center',
@@ -161,17 +189,17 @@ const Styles = StyleSheet.create({
     paddingHorizontal: 20,
     borderRadius: 13,
     flex: 1,
-    marginVertical: 20, 
-    elevation: 8,  
+    marginVertical: 20,
+    elevation: 8,
     shadowColor: '#00000050',
   },
   placeholderStyle: {
     fontSize: 14,
-    color: 'black'
+    color: 'black',
   },
   selectedTextStyle: {
     fontSize: 14,
-    color: 'black'
+    color: 'black',
   },
 });
 
